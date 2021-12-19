784d2322ee09cf96216bfed217558e4f3ccdcfa9
jimponz <72302204+jimponz@users.noreply.github.com>
2021-12-19 23:41:28 +0100

    Add files via upload

---
+++ ORDINA_FILE.py
@@ -0,0 +1,34 @@
+import os,shutil
+
+def create_dir(dirname):
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+
+def move_ext_to_dirs(path):
+    cont = 0
+    totalSize = 0
+    for f in os.listdir(path):
+        if not os.path.isdir(os.path.join(path,f)):
+            filename,fileext = os.path.splitext(f)
+            if fileext != ".lnk" and fileext != "" and fileext != ".ini" and fileext != ".crdownload":
+                if f != "ORDINA_FILE.py" and f != "ORDINA_FILE.exe" :
+                    totalSize += os.stat(os.path.join(path,f)).st_size
+                    ordpath = os.path.join(path,"ORDINATI")
+                    create_dir(ordpath)
+                    create_dir(os.path.join(ordpath,fileext[1:]))
+                    newpath = os.path.join(ordpath,fileext[1:],f)
+                    shutil.move(os.path.join(path,f),newpath)
+                    cont += 1
+    print("\nSpostati " + str(cont) + " files ("+ str(totalSize/1024/1024) + " Mb)"+" in " + dire)                             
+    
+
+home = os.path.expanduser('~')
+dire = os.path.join(home,"Desktop")
+move_ext_to_dirs(dire)
+
+dire = os.path.join(home,"Documents")
+move_ext_to_dirs(dire)
+
+dire = os.path.join(home,"Downloads")
+move_ext_to_dirs(dire)
+input("\nPremi INVIO per uscire")
